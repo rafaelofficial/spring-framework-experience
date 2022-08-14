e066ec86d14a935628a0fe3ee630b8bed41d5e8c
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-08-14 03:03:54 -0300

    methods constructors, getter and setter and other methods

--- src/main/java/one/digital/innovation/oop/Car.java
+++ src/main/java/one/digital/innovation/oop/Car.java
@@ -6,4 +6,44 @@ public class Car {
     String color;
     String model;
     int tankCapacity;
+
+    // constructors
+    public Car() {
+    }
+
+    public Car(String color, String model, int tankCapacity) {
+        this.color = color;
+        this.model = model;
+        this.tankCapacity = tankCapacity;
+    }
+
+    // methods getters and setters
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setTankCapacity(int tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+    // method that calculate the quantity of liter of tank capacity
+    public double totalValueTankCapacity(double valueOfFuel) {
+        return this.tankCapacity + valueOfFuel;
+    }
 }
